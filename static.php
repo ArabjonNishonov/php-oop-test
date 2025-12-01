c4c6b57bbd162870c6cdc590fc454e5e5ef9f4cc
ArabkhonDev <starc9899@gmail.com>
2025-12-01 20:56:47 +0500

    add static method and properties

--- static.php
+++ static.php
@@ -1,1 +1,30 @@
 <?php
+
+class Animal
+{
+    public $name;
+    public $speed;
+
+    public function __construct($name, $speed)
+    {
+        $this->name = $name;
+        $this->speed = $speed;
+    }
+
+    public function eat(){
+        return 'eating';
+    }
+
+    public function getSpeed(){
+        return $this->speed;
+    }
+    public static function run(){
+        return 'running';
+    }
+}
+
+$dog = new Animal('Backet', '120km');
+
+var_dump($dog->eat());
+
+var_dump(Animal::run());
